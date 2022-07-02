44b555e83240902e414ce5d46c21627f530d361e
Chris Ellis <ellis.cs@gmail.com>
2022-07-02 09:40:21 +0100

    feat: 🎸 annabelle's setup

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="bg-yellow-50 min-h-screen">
+  <main class="max-w-screen-xl pt-4 mx-auto">
+    <slot />
+  </main>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,3 @@
+<h1>Annabelle's Creativity World</h1>
+
+<h2>Do whatever you want</h2>
